5d918ebc76ff1262f8d95960d88b6e93fb02190e
stephie-spec <compwork254@gmail.com>
2025-09-20 15:36:13 +0300

    uses fetch lab

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Add your code here
+document.querySelector('form').addEventListener('submit', (event) => {
+    event.preventDefault(); 
+    
+    const userName = document.querySelector('#name').value;
+    const userEmail = document.querySelector('#email').value;
+
+    submitData(userName, userEmail);
+});
+
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email,
+    };
+
+    const configurationObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+
+    return fetch("http://localhost:3000/users", configurationObject)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Unauthorized Access'); 
+            }
+            return response.json();
+        })
+        .then((userData) => {
+            const userId = document.querySelector('#user-id');
+            userId.textContent = String(userData.id); 
+            console.log(userData);
+            return userData;
+        })
+        .catch(error => {
+            const errorMsg = document.querySelector('#errorMsg');
+            errorMsg.textContent = error.message; 
+            document.body.appendChild(errorMsg);
+        });
+}
